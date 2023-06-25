1e6ab89b3551b10b79704d556c8c0a691850006b
LeonardoMonteiroo <92556142+LeonardoMonteiroo@users.noreply.github.com>
2023-06-25 15:47:01 -0300

    add env and database

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import { randomUUID } from 'crypto'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transactions
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTPP Server Running!')
+  })
